137bc167a6f8019ae5b17a93492d85d059939dc9
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-03 11:13:27 -0500

    TestAuthController: agregando la anotion PreAuthorize para que todos los enpoints no tengan acceso

---
+++ src/main/java/com/isai/spring_security_demo/controller/TestAuthController.java
@@ -0,0 +1,28 @@
+package com.isai.spring_security_demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/auth")
+@PreAuthorize("denyAll()")
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Word.";
+    }
+
+    @GetMapping("/helloSecured")
+    public String helloSecured() {
+        return "Hello Word Secured.";
+    }
+
+    @GetMapping("/helloSecured02")
+    public String helloSecured02() {
+        return "Hello Word Secured.";
+    }
+
+}

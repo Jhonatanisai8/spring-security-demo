57c5a6653b61fffd0b40f9c11d7d60dff960594c
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-03 11:51:12 -0500

    estableciendo relaciones entre tablas

--- src/main/java/com/isai/spring_security_demo/persistens/entitys/UserEntity.java
+++ src/main/java/com/isai/spring_security_demo/persistens/entitys/UserEntity.java
@@ -1,10 +1,19 @@
 package com.isai.spring_security_demo.persistens.entitys;
 
+import java.util.HashSet;
+// import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -38,4 +47,7 @@ public class UserEntity {
 
     private Boolean credentialsNoExpire;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "id_user"), inverseJoinColumns = @JoinColumn(name = "id_role"))
+    private Set<RoleEntity> roles = new HashSet<RoleEntity>();
 }

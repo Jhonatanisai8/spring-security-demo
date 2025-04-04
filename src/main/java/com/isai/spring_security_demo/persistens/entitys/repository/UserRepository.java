32ceba62fab1d54ac87537bfe13adde44be1a022
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-04 11:48:10 -0500

    definicion del metodo findUserEntityByUserName

--- src/main/java/com/isai/spring_security_demo/persistens/entitys/repository/UserRepository.java
+++ src/main/java/com/isai/spring_security_demo/persistens/entitys/repository/UserRepository.java
@@ -2,7 +2,6 @@ package com.isai.spring_security_demo.persistens.entitys.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +12,6 @@ public interface UserRepository
         extends CrudRepository<UserEntity, Long> {
     Optional<UserEntity> findUserEntityByUserName(String userName);
 
-    @Query("Select u from UserEntity u where u.userName = ?")
-    Optional<UserEntity> findUser(String userName);
+    // @Query("Select u from UserEntity u where u.userName = ?")
+    // Optional<UserEntity> findUser(String userName);
 }

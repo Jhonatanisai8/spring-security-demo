a20476e50752d4f2259b90d66427fd8702e2d2d1
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-04 11:47:27 -0500

    controlador TestAuthController: nuevos enpoinst para pruebas

--- src/main/java/com/isai/spring_security_demo/controller/TestAuthController.java
+++ src/main/java/com/isai/spring_security_demo/controller/TestAuthController.java
@@ -3,29 +3,44 @@ package com.isai.spring_security_demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/auth")
 @PreAuthorize("denyAll()")
 public class TestAuthController {
-
-    @GetMapping("/hello")
-    @PreAuthorize("permitAll()")
-    public String hello() {
-        return "Hello Word.";
-    }
-
-    @GetMapping("/helloSecured")
+    @GetMapping("/get")
     @PreAuthorize("hasAuthority('READ')")
-    public String helloSecured() {
-        return "Hello Word Secured.";
+    public String helloGet() {
+        return "Hello World - GET";
     }
     
-    @GetMapping("/helloSecured02")
+    @PostMapping("/post")
     @PreAuthorize("hasAuthority('CREATED')")
-    public String helloSecured02() {
-        return "Hello Word Secured.";
+    public String helloPost() {
+        return "Hello World - POST";
+    }
+    
+    @PutMapping("/put")
+    @PreAuthorize("hasAuthority('UPDATE')")
+    public String helloPut() {
+        return "Hello World - PUT";
+    }
+    
+    @DeleteMapping("/delete")
+    @PreAuthorize("hasAuthority('DELETE')")
+    public String helloDelete() {
+        return "Hello World - DELETE";
+    }
+    
+    @PatchMapping("/patch")
+    @PreAuthorize("hasAuthority('REFACTOR')")
+    public String helloPatch() {
+        return "Hello World - PATCH";
     }
 
 }

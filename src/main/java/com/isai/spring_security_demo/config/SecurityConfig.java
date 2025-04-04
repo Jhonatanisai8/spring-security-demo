5c42ff59942f3075cdbf8ae7ef443a26969a64e3
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-04 11:46:59 -0500

    eliminacion del metodo userDetailsService

--- src/main/java/com/isai/spring_security_demo/config/SecurityConfig.java
+++ src/main/java/com/isai/spring_security_demo/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package com.isai.spring_security_demo.config;
 
-import java.util.List;
-
 // import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,12 +12,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.isai.spring_security_demo.services.UserDetailsServiceImp;
@@ -31,22 +24,25 @@ import com.isai.spring_security_demo.services.UserDetailsServiceImp;
 public class SecurityConfig {
 
     // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    //     return httpSecurity
-    //             .csrf(crsf -> crsf.disable())
-    //             .httpBasic(Customizer.withDefaults())
-    //             .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // no guardar la sesion en menoria
-    //             .authorizeHttpRequests(http  -> {
-    //                 //configurar los enpoints publicos
-    //                 http.requestMatchers(HttpMethod.GET,"/auth/hello");
-    //                 //configurar los enpoints privados
-    //                 http.requestMatchers(HttpMethod.GET,"/auth/helloSecured")
-    //                 .hasAuthority("CREATED");
-    //                 //configurar los enpoints no especificados
-    //                 // http.anyRequest().denyAll(); //rechaza todo 
-    //                 http.anyRequest().authenticated(); //permite mientras estas autenticado
-    //             })
-    //             .build();
+    // public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+    // throws Exception {
+    // return httpSecurity
+    // .csrf(crsf -> crsf.disable())
+    // .httpBasic(Customizer.withDefaults())
+    // .sessionManagement(sesion ->
+    // sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // no guardar
+    // la sesion en menoria
+    // .authorizeHttpRequests(http -> {
+    // //configurar los enpoints publicos
+    // http.requestMatchers(HttpMethod.GET,"/auth/hello");
+    // //configurar los enpoints privados
+    // http.requestMatchers(HttpMethod.GET,"/auth/helloSecured")
+    // .hasAuthority("CREATED");
+    // //configurar los enpoints no especificados
+    // // http.anyRequest().denyAll(); //rechaza todo
+    // http.anyRequest().authenticated(); //permite mientras estas autenticado
+    // })
+    // .build();
     // }
 
     @Bean
@@ -54,9 +50,14 @@ public class SecurityConfig {
         return httpSecurity
                 .csrf(crsf -> crsf.disable())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // no guardar la sesion en menoria
+                .sessionManagement(sesion -> sesion.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // no
+                                                                                                            // guardar
+                                                                                                            // la sesion
+                                                                                                            // en
+                                                                                                            // menoria
                 .build();
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
@@ -73,20 +74,20 @@ public class SecurityConfig {
 
     // @Bean
     // public UserDetailsService userDetailsService() {
-    //     List<UserDetails> userDetails = List.of(
-    //             User
-    //                     .withUsername("jhona")
-    //                     .password("1234")
-    //                     .roles("ADMIN")
-    //                     .authorities("READ", "CREATED")
-    //                     .build(),
-    //             User
-    //                     .withUsername("eli")
-    //                     .password("1234")
-    //                     .roles("USER")
-    //                     .authorities("READ")
-    //                     .build());
-    //     return new InMemoryUserDetailsManager(userDetails);
+    // List<UserDetails> userDetails = List.of(
+    // User
+    // .withUsername("jhona")
+    // .password("1234")
+    // .roles("ADMIN")
+    // .authorities("READ", "CREATED")
+    // .build(),
+    // User
+    // .withUsername("eli")
+    // .password("1234")
+    // .roles("USER")
+    // .authorities("READ")
+    // .build());
+    // return new InMemoryUserDetailsManager(userDetails);
     // }
 
     @Bean
